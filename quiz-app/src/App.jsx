205db4f126527ed79d694b48f3f84e8334e3ac8c
Mentesnot Assefa <mentesnotassefa0@gmail.com>
2025-03-20 21:01:23 +0000

    the home page setup updated

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-red-600 text-white">
-        <h1 className="text-3xl font-bold p-4 text-white">Quiz App</h1>
+      <div className="min-h-screen bg-gray-200 text-black">
+        <h1 className="text-3xl font-bold p-4 text-black">Quiz App</h1>
         <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
